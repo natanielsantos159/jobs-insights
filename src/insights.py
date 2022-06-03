0aad3108460629497b88589e7ae88a5ee549546f
Nataniel Santos <nathan.santos159@hotmail.com>
2022-06-02 23:17:13 -0300

    Requisito 6 concluido

--- src/insights.py
+++ src/insights.py
@@ -40,7 +40,9 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
+
+    filtered = [job for job in jobs if job["job_type"] == job_type]
+    return filtered
 
 
 def get_unique_industries(path):
